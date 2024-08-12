d3b8a6a2390bb2065b442fb6e3ab970148662143
adrian92612 <adrian.villamin0612@gmail.com>
2024-08-13 00:39:59 +0800

    added google provider, login/logout actions. Not functional yet

---
+++ app/lib/actions.ts
@@ -0,0 +1,12 @@
+"use server";
+
+import { signIn, signOut } from "@/auth";
+
+export const login = async (formData: FormData) => {
+  const provider = formData.get("action") as string | null;
+  if (provider) await signIn(provider, { redirectTo: "/dashboard" });
+};
+
+export const logout = async () => {
+  await signOut({ redirectTo: "/" });
+};
--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { login } from "@/app/lib/actions";
 
 export const LoginForm = () => {
   return (
     <div>
-      <form action="" className="flex flex-col">
+      <form action={login} className="flex flex-col">
         <label htmlFor="email">Email Address</label>
         <input type="text" name="email" id="email" placeholder="john.doe@example.com" />
         <label htmlFor="password">Password</label>
@@ -16,7 +17,9 @@ export const LoginForm = () => {
           <span className="px-4 text-sm text-gray-500">or</span>
           <div className="flex-grow h-px bg-gray-300"></div>
         </div>
-        <button>Login using Google</button>
+        <button name="action" value="google">
+          Login using Google
+        </button>
         <div>
           <Link href="/register">Don&apos;t have account? Register here.</Link>
         </div>
